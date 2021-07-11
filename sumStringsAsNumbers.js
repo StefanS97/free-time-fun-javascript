e4f7c8d49728f410d33fb4d3928a516f94f1117a
Stefan Stojkov <stefan.stojkov97@gmail.com>
2021-07-11 02:34:29 +0200

    Update sumStringsAsNumbers.js

--- sumStringsAsNumbers.js
+++ sumStringsAsNumbers.js
@@ -1,3 +1,5 @@
 function sumStrings(a,b) {
   return ((!isNaN(parseInt(a)) ? BigInt(a):'') + (!isNaN(parseInt(b)) ? BigInt(b):'')).toString();
 }
+
+// Really happy of the code after I've seen the other solutions when I solved the Kata.
